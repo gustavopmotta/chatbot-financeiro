548549130722568a4fe1d63ec273dbcd7dff3154
KarolineLima <queirozzzks@gmail.com>
2026-05-20 21:30:06 -0300

    adicinando frsases extras

--- bot.py
+++ bot.py
@@ -17,22 +17,32 @@ def buscar_respostas():
     return perguntas, respostas
 
 def iniciar_financeiro_fashionflow():
+    # Carregando as variáveis do seu CSV para dentro do bot
+    perguntas, respostas = buscar_respostas()
+
     print("FashionFlow: Ola, tudo bem? Somos a FashionFlow!")
     print("Financeiro: Voce esta conversando com o setor Financeiro. Como podemos te ajudar?")
     print("(Digite 'sair' para encerrar)")
 
     while True:
-        
         msg = input("\nVoce: ").lower().strip()
 
-        perguntas, respostas = buscar_respostas()
+        palavras_saida = ["ok", "certo", "obrigado", "valeu", "vlw", "ajudou", "pronto", "sair"]
 
+        if msg in palavras_saida:
+            print("Financeiro: Atendimento encerrado. A FashionFlow agradece! Tenha um otimo dia.")
+            break
+
+        encontrou = False
         for categoria, palavras in perguntas.items():
             if any(palavra in msg for palavra in palavras):
                 print(f"Financeiro: {respostas[categoria]}")
+                encontrou = True
                 break
-        else:
-            print("FashionFlow: Desculpe, nao entendi. Tente palavras como: boleto, pagamento, estorno ou nota fiscal.")
 
+        if not encontrou:
+            print("FashionFlow: Desculpe, nao entendi. Tente palavras como: boleto, pagamento, estorno, unidade ou correios.")
+
+# Correção dos underlines do dunder main para o código executar
 if __name__ == "__main__":
     iniciar_financeiro_fashionflow()
